451141750b020a7ef0643a7107440a331235ac70
vincent <vguzman812@gmail.com>
2023-05-25 20:11:24 -0400

    Delete validate username and email functions form register form class

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -75,16 +75,6 @@ class RegisterForm(FlaskForm):
 	])
 	submit = SubmitField("Sign Me Up!")
 
-	def validate_email(self, field):
-		if field.data != self.user.email and \
-				User.query.filter_by(email=field.data).first():
-			raise ValidationError('Email already registered.')
-
-	def validate_username(self, field):
-		if field.data != self.user.username and \
-				User.query.filter_by(username=field.data).first():
-			raise ValidationError('Username already in use.')
-
 
 class ResetPasswordForm(FlaskForm):
 	password = PasswordField('New Password', validators=[
